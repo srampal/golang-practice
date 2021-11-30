f83837bcd948db13dfecb70791a51d33eb704a48
Sanjeev Rampal <srampal@redhat.com>
2021-11-29 23:04:50 -0500

    2nd commit Fibonacci exercise

---
+++ ex2/ex2.go
@@ -0,0 +1,36 @@
+package ex2
+
+import (
+	"errors"
+	"log"
+)
+
+//Exercise Fibonacci calculator. Takes user input n, returns Fib(n)
+func Exercise(n uint32) (fib uint32, err error) {
+
+	var i, fibMinus1, fibMinus2 uint32
+
+	if n > 100 {
+		log.Printf("Received invalid input %d\n", n)
+		err = errors.New("Invalid input!")
+	}
+
+	if n == 0 {
+		fib = 0
+		return
+	}
+	if n == 1 {
+		fib = 1
+		return
+	}
+
+	fibMinus2 = 0
+	fibMinus1 = 1
+
+	for i = 2; i <= n; i++ {
+		fib = fibMinus2 + fibMinus1
+		fibMinus2 = fibMinus1
+		fibMinus1 = fib
+	}
+	return
+}
